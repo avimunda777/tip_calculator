431f6a289efb105c83d010568d80c0e76603883c
Avinash Singh Munda <96285074+avimunda777@users.noreply.github.com>
2023-06-13 16:45:46 +0530

    Update tip_calculator_

--- cal_project.py
+++ cal_project.py
@@ -1,10 +1,14 @@
 # Tip calculator
 # Each person should pay (150.00 / 5) * 1.12
-# Round the result to 2 decimal places = 33.60
 print('Welcome to the tip calculator!')
 bill = float(input('What was the bill? $'))
 
+# Round the result to 2 decimal places = 33.60
 tip = int(input('How much tip would you like to give? 10, 12, or 15?' ))
 people = int(input('How many people to split the bill?'))
-bill_with_tip = tip / 100 * bill + bill
-print(bill_with_tip)
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f'Each person should pay: $ {final_amount}')
